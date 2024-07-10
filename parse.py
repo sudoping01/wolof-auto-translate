d590b18c774acb442682be9fd5b47faf344e1065
sudoping01 <sudoping01@gmail.com>
2024-07-10 03:07:45 +0000

    using sys.args to capture info

--- parse.py
+++ parse.py
@@ -1,22 +1,20 @@
 import json 
-file_names= ["english_wolof_0.json", 
-             "english_wolof_1.json",
-             "english_wolof_2.json",
-             "english_wolof_3.json",
-             "english_wolof_4.json",
-             "english_wolof_5.json"
-             ]
+import sys 
 
 english_wolof= {}
-
+file_names = sys.argv[1:] 
 
 for file in file_names : 
     with open(file,"r") as f:
-        sub_data = json.load(f)
-        english_wolof.update(sub_data)
-        f.close()
+        try :
+            sub_data = json.load(f)
+            english_wolof.update(sub_data)
+            f.close()
+        except Exception as e : 
+            print(f"{file} : {e}")
+            exit()
 
-with open("english_wolof_total.json", "w") as file:
+with open("pair_english_wolof.json", "w") as file:
     json.dump(english_wolof,file)
     file.close()
 
@@ -28,7 +26,7 @@ for pair in english_wolof:
     english_wolof_format.append({"en": pair[0], "wo" : pair[1]})
 
 
-with open("english_wolof_formated.json", "a") as file : 
+with open("english_wolof_huggingface_dataset_format.json", "a") as file : 
     json.dump(english_wolof_format,file)
 
 
